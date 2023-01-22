e24018e81e11722c5867ea0140f07235033da1c0
nr <99275945+giterhaber@users.noreply.github.com>
2023-01-23 06:20:04 +0800

    Update status.js

--- assets/status.js
+++ assets/status.js
@@ -1,7 +1,8 @@
 //login
 
 function statusLOG() {
-    const status = localStorage.getItem('status')
+    // const status = localStorage.getItem('status')
+    const status = getCookie('status')
     if(!status) {
         location.href = 'index.html'
     } else {
@@ -13,7 +14,26 @@ statusLOG()
 
 $(document).ready(function() {
     
-    if (localStorage.getItem('status') == 'on') {
+    if (getCookie('status') == 'on') {
         $('#passcode').hide()
     }
 })
+
+
+function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i <ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
+
+console.log(getCookie('status'));
